a49c448ae3d303ecc62f051c2a68d892885b8d9a
KumarNavin <navins398@gmail.com>
2022-12-29 22:44:03 +0530

    uc4 completed

--- src/com/bridgelabz/EmpWage.java
+++ src/com/bridgelabz/EmpWage.java
@@ -3,25 +3,25 @@ package com.bridgelabz;
 public class EmpWage {
     public static void main(String[] args) {
         System.out.println("Welcome");
-        int IS_FULL_TIME=1;
-        int IS_PART_TIME=2;
-        int Emp_Per_Hr_Rate=20;
+        final int IS_FULL_TIME=1;
+        final   int IS_PART_TIME=2;
+        final   int Emp_Wage_Per_Hr=20;
         //variables
         int Emp_Hr=0;
         int Emp_Wage=0;
-        int Emp_check=(int)(Math.random()*3);
-        // double Emp_check=Math.floor(Math.random() * 10) % 3;
-        if (Emp_check==IS_FULL_TIME){
-            System.out.println("Employee work full time");
-            Emp_Hr=8;
-        } else if (Emp_check==IS_PART_TIME) {
-            System.out.println("Employee work part time");
-            Emp_Hr=4;
+        int Emp_Check=(int)(Math.random()*3);
+
+        switch (Emp_Check) {
+            case IS_FULL_TIME:
+                Emp_Hr = 8;
+                break;
+            case IS_PART_TIME:
+                Emp_Hr = 4;
+                break;
+            default:
+                Emp_Hr = 0;
         }
-        else {
-            System.out.println("Employee Not worked");
-        }
-        Emp_Wage=Emp_Hr*Emp_Per_Hr_Rate;
+        Emp_Wage=Emp_Hr*Emp_Wage_Per_Hr;
         System.out.println("Employee wage = " +Emp_Wage);
     }
     }
