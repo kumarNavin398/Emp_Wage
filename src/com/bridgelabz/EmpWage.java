1d4c1df3180b02982b956f008891fbbdba75c1a1
KumarNavin <navins398@gmail.com>
2022-12-29 21:26:01 +0530

    uc2 completed

--- src/com/bridgelabz/EmpWage.java
+++ src/com/bridgelabz/EmpWage.java
@@ -3,15 +3,24 @@ package com.bridgelabz;
 public class EmpWage {
     public static void main(String[] args) {
         System.out.println("Welcome");
-               int IS_FULL_TIME = 1;
-                //OR double Emp_Check=Math.floor(Math.random() * 10) % 2;
-                int Emp_Check=(int)(Math.random()*2);
-                if(Emp_Check == IS_FULL_TIME){
-                    System.out.println("present");
-                }
-                else {
-                    System.out.println("Absent");
-                }
-            }
+        final int IS_FULL_TIME =1;
+        final int Emp_Per_hour_Rate = 20;
+        //variables
+        int Emp_hr =0;
+        int  Emp_wage=0;
+        double Emp_check=Math.floor(Math.random()*10) % 2;
+        // int Emp_Check= (int)(10+Math.random()*20);
+        if(Emp_check== IS_FULL_TIME){
+            System.out.println("Employee is present");
+            Emp_hr=8;
+        }
+        else {
+            System.out.println("employee is Absent");
+        }
+        Emp_wage =Emp_hr * Emp_Per_hour_Rate;
+        System.out.println("Employee wage = " +Emp_wage);
     }
+
+            }
+    
 
