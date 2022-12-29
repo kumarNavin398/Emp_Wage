3758a39a54e44b557d28e994c494b2c012707f9a
KumarNavin <navins398@gmail.com>
2022-12-29 23:02:21 +0530

    uc5 completed

--- src/com/bridgelabz/EmpWage.java
+++ src/com/bridgelabz/EmpWage.java
@@ -1,31 +1,36 @@
 package com.bridgelabz;
 
-public class EmpWage {
-    public static void main(String[] args) {
-        System.out.println("Welcome");
-        final int IS_FULL_TIME=1;
-        final   int IS_PART_TIME=2;
-        final   int Emp_Wage_Per_Hr=20;
-        //variables
-        int Emp_Hr=0;
-        int Emp_Wage=0;
-        int Emp_Check=(int)(Math.random()*3);
+import java.util.Random;
 
-        switch (Emp_Check) {
-            case IS_FULL_TIME:
-                Emp_Hr = 8;
+public class EmpWage {
+
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation");
+        Random random = new Random();
+        int attendance = random.nextInt(3);
+
+        int wagePerHour = 20;
+        int fullDayHours = 8;
+        int partTimeHours = 4;
+        int wagePerDay = 0;
+
+        switch (attendance) {
+            case 0:
+                System.out.println("Employee Absent.");
                 break;
-            case IS_PART_TIME:
-                Emp_Hr = 4;
+            case 1:
+                wagePerDay = partTimeHours * wagePerHour;
                 break;
             default:
-                Emp_Hr = 0;
-        }
-        Emp_Wage=Emp_Hr*Emp_Wage_Per_Hr;
-        System.out.println("Employee wage = " +Emp_Wage);
-    }
-    }
+                wagePerDay = fullDayHours * wagePerHour;
 
+        }
+
+        System.out.println("Employee Wage Per Day: " + wagePerDay);
+
+    }
+}
             
     
 
